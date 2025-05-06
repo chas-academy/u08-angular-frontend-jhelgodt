19f2a2440267ad921cb4edd508523ae25c9cc26f
Joakim Helgodt <joakim.helgodt@gmail.com>
2025-05-06 12:46:49 +0200

    feat: add route to display BookListComponent at root path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: BookListComponent }];
