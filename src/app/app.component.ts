0c18ecfbdfdb4b83624990d03d6c0ded2991d69a
Joakim Helgodt <joakim.helgodt@gmail.com>
2025-05-06 12:57:04 +0200

    feat: display BookFormComponent above router-outlet in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookFormComponent } from './components/book-form/book-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, BookFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'temp-project';
