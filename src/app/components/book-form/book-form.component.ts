a2a91a79beadf5c0c1613abe1f0e06ad675f388d
Joakim Helgodt <joakim.helgodt@gmail.com>
2025-05-06 12:53:48 +0200

    feat: implement book creation logic with BookService in BookFormComponent

--- src/app/components/book-form/book-form.component.ts
+++ src/app/components/book-form/book-form.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Book } from '../../models/book.model';
+import { BookService } from '../../services/book.service';
 
 @Component({
   selector: 'app-book-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './book-form.component.html',
-  styleUrl: './book-form.component.scss'
+  styleUrls: ['./book-form.component.scss'],
 })
 export class BookFormComponent {
+  book: Book = {
+    title: '',
+    author: '',
+    publishedYear: new Date().getFullYear(),
+    genre: '',
+  };
 
+  constructor(private bookService: BookService) {}
+
+  onSubmit(): void {
+    this.bookService.createBook(this.book).subscribe({
+      next: (newBook) => {
+        console.log('Book created:', newBook);
+        this.book = {
+          title: '',
+          author: '',
+          publishedYear: new Date().getFullYear(),
+          genre: '',
+        };
+      },
+      error: (err) => {
+        console.error('Failed to create book:', err);
+      },
+    });
+  }
 }
