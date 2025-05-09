c25ea342cc294a73e0231d5b66ecd0624f2b025f
Joakim Helgodt <joakim.helgodt@gmail.com>
2025-05-09 09:51:02 +0200

    feat: add input property to BookListComponent to update list with new books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BookFormComponent } from './components/book-form/book-form.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 import { Book } from './models/book.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BookFormComponent],
+  imports: [RouterOutlet, BookFormComponent, BookListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  newBook?: Book;
+
   onBookAdded(newBook: Book): void {
     console.log('New book added:', newBook);
-    // Här kan vi senare lägga logik för att uppdatera listan
+    this.newBook = newBook; // Skickas vidare till BookListComponent
   }
 }
--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BookService } from '../../services/book.service';
 import { Book } from '../../models/book.model';
-import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
@@ -9,12 +10,23 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnChanges {
+  @Input() newBook?: Book; // 👈 Tar emot ny bok
+
   books: Book[] = [];
 
-  constructor(private bookService: BookService) {}
+  constructor(private bookService: BookService) {
+    this.loadBooks();
+  }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['newBook'] && changes['newBook'].currentValue) {
+      console.log('New book received:', changes['newBook'].currentValue);
+      this.books.push(changes['newBook'].currentValue);
+    }
+  }
+
+  private loadBooks(): void {
     this.bookService.getAllBooks().subscribe({
       next: (data) => {
         this.books = data;
